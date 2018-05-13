79689de8e0aaa5c2729b8df2f98726b8a7e977d7
Kyoma612424 <mou.61.2424@gmail.com>
2018-05-14 07:54:05 +0900

    add flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
+    flash[:success]="Welcome to the Sample App"
     @user = User.find(params[:id])
   end
 
@@ -10,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #save できた時の応答
+      redirect_to @user
     else
       render'new'
     end
