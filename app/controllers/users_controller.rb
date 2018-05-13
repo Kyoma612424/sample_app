bb9fe2a6ae1fd61629f335f94c041004ef5c562a
Kyoma612424 <mou.61.2424@gmail.com>
2018-05-14 08:02:02 +0900

    typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   def show
-    flash[:success]="Welcome to the Sample App"
     @user = User.find(params[:id])
   end
 
@@ -11,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success]="Welcome to the Sample App"
       redirect_to @user
     else
       render'new'
