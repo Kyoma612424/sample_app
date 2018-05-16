d3a8b1b7e61ce21dab9943276530e0ad38e43822
Kyoma612424 <mou.61.2424@gmail.com>
2018-05-16 09:33:27 +0900

    show dropdown menu in the header for logged in users.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,17 @@
 module SessionsHelper
+  
+  #渡されたユーザーでログインする
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  #現在のユーザーを返す
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  #ユーザーがログインしていればtrueを返す
+  def logged_in? 
+    !current_user.nil?
+  end
 end
