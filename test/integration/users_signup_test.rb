e4bb3514f71f731f87308419475c71aa287cb1b1
Kyoma612424 <mou.61.2424@gmail.com>
2018-05-23 09:18:55 +0900

    8.2.5 ユーザー登録時にログイン

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -29,9 +29,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 			password: "password",
 			password_confirmation: "password" } }
     end
-    follow_redirect!
-    
+    follow_redirect!    
     assert_template 'users/show'
+    assert is_logged_in?
     assert_not flash.empty?
   end
 end
